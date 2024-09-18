114e87abb9ded429249202e798012f607be7ebea
TanS36 <karimsarkulov312@gmail.com>
2024-09-18 11:50:17 +0600

    new mashina

---
+++ Math.py
@@ -0,0 +1,5 @@
+
+OurMoney = 20000
+Proc = 7
+Sollution = 20000 * (1 + 7 / 100) * 5 
+print(Sollution) 
---
+++ Thetest.py
@@ -0,0 +1,10 @@
+def ChessGame(Move1 , Move2):
+    Let_x , Lety = Move1
+    Set_x , Sety = Move2
+    if Let_x  == Set_x or Lety == Sety or abs(Let_x - Set_x) == abs(Let_x - Set_x):
+        return True
+    else:
+        False
+Move1 = 6,5
+Move2 = 4, 4
+print(ChessGame )
